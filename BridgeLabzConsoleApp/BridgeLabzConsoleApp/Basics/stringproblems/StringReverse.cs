e14fd1417a3a60d3a3a36231e2fe803c9ad97843
Karthikkm1925 <Karthikkm1925@gmail.com>
2025-11-12 16:10:34 +0530

    Added StringProblems - StringReverse and PalindromeCheck programs

---
+++ BridgeLabzConsoleApp/BridgeLabzConsoleApp/Basics/stringproblems/StringReverse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BridgeLabzConsoleApp.Basics.stringproblems
+{
+    public class StringReverse
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+
+            string reversed = ReverseString(input);
+
+            Console.WriteLine($"Reversed string: {reversed}");
+            Console.ReadLine();
+
+        }
+
+        static string ReverseString(string str)
+        {
+            string reversed = "";
+
+            // Loop from last index to first
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reversed += str[i]; // build the reversed string character by character
+            }
+
+            return reversed;
+        }
+    }
+}
