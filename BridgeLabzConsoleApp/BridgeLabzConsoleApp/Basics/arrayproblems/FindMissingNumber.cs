ef61bfea7759ab1762c64499a4f49879b08e7efa
Karthikkm1925 <Karthikkm1925@gmail.com>
2025-11-14 16:47:46 +0530

    Added string and array programs

---
+++ BridgeLabzConsoleApp/BridgeLabzConsoleApp/Basics/arrayproblems/FindMissingNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BridgeLabzConsoleApp.Basics.arrayproblems
+{
+    public class FindMissingNumber
+    {
+        public static void Main()
+        {
+            int[] numbers = { 1, 2, 4, 5, 6 };
+
+            int n = numbers.Length + 1; // total numbers if none missing
+            int expectedSum = n * (n + 1) / 2;
+
+            int actualSum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                actualSum += numbers[i];
+            }
+
+            int missingNumber = expectedSum - actualSum;
+            Console.WriteLine("Missing number: " + missingNumber);
+        }
+    }
+}
---
+++ BridgeLabzConsoleApp/BridgeLabzConsoleApp/Basics/arrayproblems/MoveZerosToEnd.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BridgeLabzConsoleApp.Basics.arrayproblems
+{
+    public class MoveZerosToEnd
+    {
+        public static void Main()
+        {
+            int[] numbers = { 1, 0, 2, 0, 4, 5, 0, 6 };
+
+            Console.Write("Array after moving zeros to end: ");
+
+            // First, print all non-zero numbers
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] != 0)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+            }
+
+            // Then, print all zeros
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == 0)
+                {
+                    Console.Write("0 ");
+                }
+            }
+        }
+    }
+}
