de0a57275dae54cb3e9fc0d6e417d882d64f9983
Karthikkm1925 <Karthikkm1925@gmail.com>
2025-12-02 14:54:06 +0530

    Delegates Demo

---
+++ BridgeLabzConsoleApp/BridgeLabzConsoleApp/advoops/deligates/DeligateDemo1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BridgeLabzConsoleApp.advoops.deligates
+{
+    public delegate void SayDeligate();
+    public class DeligateDemo1
+    {
+        static void SayHello()
+        {
+            Console.WriteLine("Hey there...!");
+        }
+
+        static void MethodOne()
+        {
+            Console.WriteLine("I am Method one");
+        }
+
+        static void MethodTwo()
+        {
+            Console.WriteLine("I am Method two");
+        }
+
+        public static void Main()
+        {
+
+            //SayDeligate sd = new SayDeligate(DeligateDemo1.SayHello);
+            //sd();
+
+            //SayDeligate sd = SayHello;
+            //sd();
+
+            SayDeligate sd;
+            sd = SayHello;
+            sd += MethodOne;
+            sd += MethodTwo;
+           // sd();
+           sd.Invoke();
+        }
+        
+    }
+}
